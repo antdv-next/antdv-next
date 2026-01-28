3297bbfa47f9326bab3a0a3cf2304037922ef5df
aibayanyu20 <aibayanyu@qq.com>
2026-01-28 16:19:57 +0800

    ci: update workflow add test ci

--- packages/cssinjs/tests/setup.ts
+++ packages/cssinjs/tests/setup.ts
@@ -1,5 +1,5 @@
 import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
-import matchers from '@testing-library/jest-dom/matchers'
+import * as matchers from '@testing-library/jest-dom/matchers'
 import { expect } from 'vitest'
 
 declare module 'vitest' {
@@ -7,5 +7,6 @@ declare module 'vitest' {
     extends jest.Matchers<void, T>,
     TestingLibraryMatchers<T, void> {}
 }
+const _matchers = matchers?.default || matchers
 
-expect.extend(matchers)
+expect.extend(_matchers)
