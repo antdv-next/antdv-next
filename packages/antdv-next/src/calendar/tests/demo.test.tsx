6679912e85a1ba1e9a069c7f457e1b40bbb2b16d
Darkingtail <1603774836@qq.com>
2026-02-13 09:18:13 +0800

    test(calendar): fix demo snapshot timestamp instability
    Add setMockDate to demo.test.tsx so snapshots use fixed date
    instead of current time, preventing CI failures.

--- packages/antdv-next/src/calendar/tests/demo.test.tsx
+++ packages/antdv-next/src/calendar/tests/demo.test.tsx
@@ -1,3 +1,13 @@
+import { afterAll, beforeAll } from 'vitest'
 import demoTest from '/@tests/shared/demoTest'
+import { resetMockDate, setMockDate } from '/@tests/utils'
+
+beforeAll(() => {
+  setMockDate()
+})
+
+afterAll(() => {
+  resetMockDate()
+})
 
 demoTest('calendar')
