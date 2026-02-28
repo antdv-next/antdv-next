d39940a186f56b88649a4d2128b5e4076a4dd470
carl-chen <ccheartszzz@gmail.com>
2026-02-28 15:06:30 +0800

    fix(auto-complete): fix default display of custom input placeholder

--- packages/antdv-next/src/auto-complete/index.tsx
+++ packages/antdv-next/src/auto-complete/index.tsx
@@ -203,6 +203,7 @@ const InternalAutoComplete = defineComponent<
       }
 
       const getInputElement = customizeInput ? () => customizeInput : undefined
+      const customizeInputPlaceholder = customizeInput?.props?.placeholder
 
       let optionChildren: any = []
       if (hasSelectOptions) {
@@ -303,6 +304,9 @@ const InternalAutoComplete = defineComponent<
       }
 
       const selectProps: Record<string, any> = omit(props, omitKeys)
+      if (selectProps.placeholder === undefined && customizeInputPlaceholder !== undefined) {
+        selectProps.placeholder = customizeInputPlaceholder
+      }
       const onAttrs = {
         onSelect: (value: any, option: any) => {
           emit('select', value, option)
