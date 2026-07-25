c77a08ae74eccc0d2b97a6639e8ba21e1ad0643d
Bin <fuwb0407@gmail.com>
2026-07-25 17:14:29 +0800

    feat(docs): supplement the upload demo

--- docs/src/pages/components/upload/demo/drag-sorting.vue
+++ docs/src/pages/components/upload/demo/drag-sorting.vue
@@ -1,21 +1,19 @@
 <docs lang="zh-CN">
-使用 `itemRender` ，我们可以使用原生 drag 来实现对上传列表拖拽排序。
+使用 `@dnd-kit/vue` 实现上传列表的拖拽排序。
 </docs>
 
 <docs lang="en-US">
-By using `itemRender`, we can integrate upload with drag to implement drag sorting of uploadList.
-
-```css
-.is-dragging a {
-  pointer-events: none;
-}
-```
+Drag sorting of uploadList by using `@dnd-kit/vue`.
 </docs>
 
 <script setup lang="ts">
+import type { DragEndEvent } from '@dnd-kit/vue'
 import type { UploadEmits, UploadFile } from 'antdv-next'
 import { UploadOutlined } from '@antdv-next/icons'
-import { ref } from 'vue'
+import { PointerActivationConstraints } from '@dnd-kit/dom'
+import { DragDropProvider, PointerSensor } from '@dnd-kit/vue'
+import { isSortable, useSortable } from '@dnd-kit/vue/sortable'
+import { defineComponent, h, ref } from 'vue'
 
 const fileList = ref<UploadFile[]>([
   {
@@ -49,72 +47,88 @@ const fileList = ref<UploadFile[]>([
   },
 ])
 
-const draggingUid = ref<string | null>(null)
+const SortableWrapper = defineComponent({
+  props: {
+    id: { type: String, required: true },
+    index: { type: Number, required: true },
+  },
+  setup(props, { slots }) {
+    const handle = ref<HTMLElement>()
+    const { isDragging } = useSortable({
+      id: () => props.id,
+      index: () => props.index,
+      element: () => handle.value?.parentElement,
+      handle,
+    })
+    return () => h('div', {
+      ref: handle,
+      class: ['upload-sort-item', { 'is-dragging': isDragging.value }],
+    }, slots.default?.())
+  },
+})
+
+const sensors = [
+  PointerSensor.configure({
+    activationConstraints: [
+      new PointerActivationConstraints.Distance({ value: 10 }),
+    ],
+  }),
+]
 
 const handleChange: UploadEmits['change'] = ({ fileList: newFileList }) => {
   fileList.value = newFileList
 }
 
-function handleDragStart(uid: string, event: DragEvent) {
-  draggingUid.value = uid
-  event.dataTransfer?.setData('text/plain', uid)
-}
-
-function handleDragEnd() {
-  draggingUid.value = null
-}
-
-function handleDrop(overUid: string, event: DragEvent) {
-  const activeUid = event.dataTransfer?.getData('text/plain')
-  if (!activeUid || activeUid === overUid) {
+function handleDragEnd(event: DragEndEvent) {
+  if (event.canceled)
     return
+
+  const { source } = event.operation
+
+  if (isSortable(source)) {
+    const { initialIndex, index } = source
+
+    if (initialIndex !== index) {
+      const newList = [...fileList.value]
+      const [moved] = newList.splice(initialIndex, 1) as [UploadFile]
+      newList.splice(index, 0, moved)
+      fileList.value = newList
+    }
   }
-  const activeIndex = fileList.value.findIndex(file => file.uid === activeUid)
-  const overIndex = fileList.value.findIndex(file => file.uid === overUid)
-  if (activeIndex < 0 || overIndex < 0) {
-    return
-  }
-  const nextList = fileList.value.slice()
-  const [moved] = nextList.splice(activeIndex, 1)
-  nextList.splice(overIndex, 0, moved)
-  fileList.value = nextList
 }
 </script>
 
 <template>
-  <a-upload
-    v-model:file-list="fileList"
-    action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
-    @change="handleChange"
-  >
-    <template #itemRender="{ originNode, file }">
-      <div
-        class="upload-sort-item"
-        :class="{ 'is-dragging': draggingUid === file.uid }"
-        draggable="true"
-        @dragstart="(event) => handleDragStart(file.uid, event)"
-        @dragover.prevent
-        @drop="(event) => handleDrop(file.uid, event)"
-        @dragend="handleDragEnd"
-      >
-        <component :is="originNode" />
-      </div>
-    </template>
-    <a-button>
-      <template #icon>
-        <UploadOutlined />
+  <DragDropProvider :sensors="sensors" @drag-end="handleDragEnd">
+    <a-upload
+      v-model:file-list="fileList"
+      action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+      @change="handleChange"
+    >
+      <template #itemRender="{ originNode, file, fileList }">
+        <SortableWrapper :id="file.uid" :index="fileList.indexOf(file)">
+          <component :is="originNode" />
+        </SortableWrapper>
       </template>
-      Click to Upload
-    </a-button>
-  </a-upload>
+      <a-button>
+        <template #icon>
+          <UploadOutlined />
+        </template>
+        Click to Upload
+      </a-button>
+    </a-upload>
+  </DragDropProvider>
 </template>
 
 <style scoped>
 .upload-sort-item {
   cursor: move;
+  user-select: none;
 }
-
-.is-dragging a {
+.upload-sort-item :deep(a) {
+  -webkit-user-drag: none;
+}
+.is-dragging :deep(a) {
   pointer-events: none;
 }
 </style>
