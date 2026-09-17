eaf256089e19067376b4bc548e82627acce13e24
dogle <15707274049@163.com>
2026-09-17 13:26:41 +0800

    refactor(app,dropdown): remove duplicate install assignment in index wrappers

--- packages/antdv-next/src/app/index.tsx
+++ packages/antdv-next/src/app/index.tsx
@@ -1,4 +1,3 @@
-import type { App as AppPlugin } from 'vue'
 import type { AppProps } from './App'
 import App_ from './App'
 import useApp from './useApp'
@@ -10,9 +9,6 @@ type CompoundedComponent = typeof App_ & {
 }
 
 const App = App_ as CompoundedComponent
-;(App as any).install = (app: AppPlugin) => {
-  app.component(App.name, App)
-}
 
 App.useApp = useApp
 
