2612ecb0ad9a39d374ed6ae5287e927854889afd
多格 <40968928+dogledogle@users.noreply.github.com>
2026-09-18 14:14:38 +0800

    refactor(app,dropdown): remove duplicate install assignment in index wrappers (#901)

--- packages/antdv-next/src/dropdown/index.tsx
+++ packages/antdv-next/src/dropdown/index.tsx
@@ -5,10 +5,6 @@ const Dropdown = InternalDropdown as typeof InternalDropdown & {
   install: (app: App) => void
 }
 
-Dropdown.install = (app: App) => {
-  app.component(Dropdown.name, Dropdown)
-}
-
 export default Dropdown
 
 export type { DropdownArrowOptions, DropdownEmits, DropdownProps, DropdownSlots } from './dropdown'
